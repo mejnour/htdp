04cfcafbc2278a2871ee9385618b77005b675d31
Pina <apina.dev@gmail.com>
2017-08-22 19:14:38 -0300

    C2E15

---
+++ exe2-15.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exe2-15) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define
+  (==> sunny friday)
+
+  (if sunny
+      (if friday
+          #true
+
+          0
+          )
+      
+      #true
+      )
+  )
+
+(define sunny #false)
+(define friday #true)
+
+(==> sunny friday)
