6b0a3a253d1a78b37d26ebdf6bbb6656326c42b6
Pina <apina.dev@gmail.com>
2017-08-22 19:04:44 -0300

    C2E13

---
+++ exe2-13.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exe2-13) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+; extrai primeiro carac de string
+
+(define
+  (string-first x)
+  
+  (string-ith x 0)
+  )
+
+(string-first "hello")
