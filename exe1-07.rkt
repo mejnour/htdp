ff92818dc6886894364c554a2c066974611251b9
Pina <apina.dev@gmail.com>
2017-08-21 22:58:49 -0300

    C1E7

---
+++ exe1-07.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exe1-07) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+; avaliando logicamente se devo
+; frequentar o shopping
+
+(define sunny #true)
+(define friday #false)
+
+(text "Should I stay or Should I go?" 25 "green")
+(or sunny friday)
