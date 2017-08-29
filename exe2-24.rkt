abf47aba907f936d6074d608c03e9fc41705153c
Pina <apina.dev@gmail.com>
2017-08-29 16:29:41 -0300

    Chap 2, Ex 24

---
+++ exe2-24.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exe2-24) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "htdp")) #f)))
+; using stepper to solve logic sentences
+
+(define
+  (==> x y)
+
+  (or (not x) y)
+  )
+
+(==> #true #false)
