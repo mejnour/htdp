c345015aa0e810136b4e47920b8125c10d98065f
Pina <apina.dev@gmail.com>
2017-09-05 01:15:22 -0300

    Chap 2, Ex 32

---
+++ exe2-32.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exe2-32) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; Ten different events that interactive
+; programs running on bio-machines in future
+; should interact with:
+;
+; 01. Hunger;
+; 02. Sleepness;
+; 03. Time to pee;
+; 04. Time to poo;
+; 05. Bodyfat %;
+; 06. Interact with sight;
+; 07. Interact with ambient sounds;
+; 08. Interact with local networks;
+; 09. Interact with taste (contact global DB for detailed contents);
+; 10. Interact with other ppls mood.
