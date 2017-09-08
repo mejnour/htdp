0b25738a9b064123db6521cc6f43be6fee60b2b2
Pina <apina.dev@gmail.com>
2017-09-08 02:26:34 -0300

    Chap 3, Ex 33

---
+++ exe3-33.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exe3-33) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; The year 2000 problem
+
+; The Y2K Bug refers to the problem faced by last cantury' society regarding the notation rollover from '99 to '00. Back then, years were mostly expressed by its last 2 digits, so, it would be confusing to differentiate 2000 and 1900. In fact, programs that relies on date parameters to properly function, should probably go nuts because of it.
+
+; Now, about the contex, it must have been a real pain in the ass having to reread and code again all high demanding complex algos. Hence, the urgence to start explainning code as plain as it gets.
