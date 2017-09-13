785176b057e0de1ddb2ec22bf2ce367a52882bf9
Pina <apina.dev@gmail.com>
2017-09-13 04:37:28 -0300

    Chap 3, Ex 39

---
+++ exe3-39.rkt
@@ -0,0 +1,79 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exe3-39) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+(require 2htdp/universe)
+
+; This program draws a car whose proportions
+; are all defined by its wheel radius.
+
+; Constants definitions
+(define WHEEL-RADIUS 5)
+(define START (* WHEEL-RADIUS 20))
+(define BACKGROUND (empty-scene START (* 5 WHEEL-RADIUS)))
+
+; Car Sampling
+(define WHEEL
+  (circle WHEEL-RADIUS "solid" "black"))
+
+(define BODY
+  (rectangle (* WHEEL-RADIUS 8) (* WHEEL-RADIUS 2) "solid" "red"))
+
+(define ROOF-TOP
+  (rectangle (* WHEEL-RADIUS 4) (* WHEEL-RADIUS 4) "solid" "red"))
+
+(define RED-FRAME
+  (rectangle (* WHEEL-RADIUS 8)
+             (* WHEEL-RADIUS 4)
+             "solid"
+             "white"))
+
+(define RED-AREA
+  (place-image ROOF-TOP
+               (/ (image-width RED-FRAME) 2)
+               (/ (image-height RED-FRAME) 2)
+               (place-image/align BODY
+                                  (image-width RED-FRAME)
+                                  (image-height RED-FRAME)
+                                  "right" "bottom"
+                                  RED-FRAME)))
+
+; Car Definitions
+(define CAR-FRAME
+  (rectangle (* WHEEL-RADIUS 8)
+             (+ (* WHEEL-RADIUS 4) (* 2 WHEEL-RADIUS))
+             "solid"
+             "white"))
+
+(define CAR
+  (place-images
+   (list WHEEL
+         WHEEL
+         RED-AREA)
+   (list (make-posn (/ (image-width CAR-FRAME) 4) (- (image-height CAR-FRAME) WHEEL-RADIUS))
+         (make-posn (* 3(/ (image-width CAR-FRAME) 4)) (- (image-height CAR-FRAME) WHEEL-RADIUS))
+         (make-posn (/ (image-width CAR-FRAME) 2) (/ (image-height CAR-FRAME) 2)))
+   CAR-FRAME))
+
+; Redering Function
+(define (render x)
+  (place-image CAR
+               x
+               WHEEL-RADIUS
+               BACKGROUND))
+
+; Counter/Clock Updater
+(define (tock x)
+  (- x 1))
+
+; Stop Condition
+(define (when t)
+  (<= t (/ (image-width BODY) 2)))
+
+; Main Funcion and World Listener
+(define (main ws)
+   (big-bang ws
+     [on-tick tock]
+     [to-draw render]
+     [stop-when when]))
+
+(main START)
